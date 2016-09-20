311f5eabf7533fd1c0c5f37047cd42e19058c83a
chauncey <chauncey182@163.com>
2016-09-20 17:23:22 +0800

    Basic view now returns minimal HTML

--- function_test.py
+++ function_test.py
@@ -13,7 +13,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
-        slef.fail('Finish the test!')
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
